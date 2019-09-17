5a6ffafeb1dda1c81f8d0e00ea54e752e69614f4
scurry222 <scurry222@gmail.com>
2019-09-17 01:33:13 +0000

    rearrange imports and requests

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """ Given employee ID, returns information about TODO list progress """
 
-import requests
-from sys import argv
-import csv
-
-
 if __name__ == '__main__':
 
-    user = "https://jsonplaceholder.typicode.com/users/" + argv[1]
-    name = requests.get(user).json().get('name')
-    req = "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1]
-    request = requests.get(req)
+    import requests
+    from sys import argv
+    import csv
+
     f_name = argv[1] + ".csv"
 
     with open(f_name, 'w', newline='') as f:
-        w = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        user = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+        name = requests.get(user).json().get('name')
+        req = "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1]
+        request = requests.get(req)
+        w = csv.writer(f, quoting=csv.QUOTE_ALL)
         for req in request.json():
             w.writerow([argv[1], name, str(req.get('completed')),
                        req.get('title')])
